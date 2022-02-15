b97a1dce5b499e24f880e043808e50a9ad1455db
pranay <90305656@ep-student.org>
2022-02-15 10:26:52 -0600

    Initial Commit

---
+++ TestAppFinal/TestAppFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppFinalApp.swift
+//  TestAppFinal
+//
+//  Created by 90305656 on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct TestAppFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
